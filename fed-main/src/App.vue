426ffd5b3a84b94e2b7134ae8e14c71c31b5883d
Aleks <aleksandar.trpkovski@gmail.com>
2022-03-12 09:35:27 +1100

    Update component name in App.vue form SearchBar to BookSearchBar

--- fed-main/src/App.vue
+++ fed-main/src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
 import HeroSection from './components/main/HeroSection.vue'
-import SearchBar from './components/global/BookSearchBar/BookSearchBar.vue'
+import BookSearchBar from './components/global/BookSearchBar/BookSearchBar.vue'
 </script>
 
 <template>
   <hero-section />
-  <search-bar />
+  <book-search-bar />
 </template>
