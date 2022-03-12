af28724443552e9fade9480a96e57166ff66e28f
Aleks <aleksandar.trpkovski@gmail.com>
2022-03-12 12:18:56 +1100

    WIP: Added draft Search list.

--- fed-main/src/App.vue
+++ fed-main/src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HeroSection from './components/main/HeroSection.vue'
 import BookSearchBar from './components/global/BookSearchBar/BookSearchBar.vue'
+import BookSearchList from './components/global/BookSearchList/BookSearchList.vue'
 </script>
 
 <template>
   <hero-section />
   <book-search-bar />
+  <book-search-list />
 </template>
