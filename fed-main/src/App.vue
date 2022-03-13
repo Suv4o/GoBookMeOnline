f4e2d9ac162ed5287d6a0220283c0762b815e55f
Aleks <aleksandar.trpkovski@gmail.com>
2022-03-13 21:12:07 +1100

    Squashed Merge from Sprint-1
    Squashed commit of the following:
    
    commit 76f3fb2b99af9ff164e51f726b284b3bf8b01496
    Author: Aleksandar Trpkovski <56303591+Suv4o@users.noreply.github.com>
    Date:   Sun Mar 13 20:38:25 2022 +1100
    
        BookSearchComponent has been completed (#5)
    
        Update the responsiveness of the BookSearchBar so can fit properly on smaller screens
    
    commit eb19a3c140d1c9916c268ab75c8694a1b0051286
    Author: Aleksandar Trpkovski <56303591+Suv4o@users.noreply.github.com>
    Date:   Sun Mar 13 19:14:41 2022 +1100
    
        Sprint 1 feature #2 search bar component (#4)
    
        Implement Vue Router 4 in our app

--- fed-main/src/App.vue
+++ fed-main/src/App.vue
@@ -1,11 +1,7 @@
-<script setup lang="ts">
-import HeroSection from './components/main/HeroSection.vue'
-import BookSearchBar from './components/global/BookSearchBar/BookSearchBar.vue'
-import BookSearchList from './components/global/BookSearchList/BookSearchList.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <hero-section />
-  <book-search-bar />
-  <book-search-list />
+  <main>
+    <router-view></router-view>
+  </main>
 </template>
