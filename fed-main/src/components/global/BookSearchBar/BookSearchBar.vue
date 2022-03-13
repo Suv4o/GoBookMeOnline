2fbd9419565cb2c57c9da587b40a48ffd2b95242
Aleks <aleksandar.trpkovski@gmail.com>
2022-03-13 23:15:29 +1100

    WIP: BookSearchList compoment made, still need needs to be refined

--- fed-main/src/components/global/BookSearchBar/BookSearchBar.vue
+++ fed-main/src/components/global/BookSearchBar/BookSearchBar.vue
@@ -10,10 +10,10 @@ import LocationInput from './LocationInput.vue'
 </script>
 
 <template>
-  <div class="flex flex-col sm:flex-row book-container">
+  <section class="flex flex-col sm:flex-row book-container mb-10 md:mb-12">
     <div class="basis-2/3 sm:mr-4 mb-2 sm:mb-0">
       <search-input />
     </div>
     <div class="basis-1/3"><location-input /></div>
-  </div>
+  </section>
 </template>
--- fed-main/src/components/global/BookSearchList/BookSearchList.vue
+++ fed-main/src/components/global/BookSearchList/BookSearchList.vue
@@ -1,85 +1,156 @@
+<script lang="ts">
+export default {
+  name: 'BookSearchList',
+}
+</script>
+
+<script setup lang="ts">
+import { defineProps, withDefaults } from 'vue'
+import { CheckCircleIcon, XCircleIcon, ChevronRightIcon } from '@heroicons/vue/solid'
+
+interface Props {
+  title: string
+  type: 'person' | 'location'
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  title: undefined,
+  type: undefined,
+})
+
+function textTimeAvailability(isAvailable: boolean) {
+  if (props.type === 'location' && isAvailable) {
+    return 'Opens at'
+  }
+  if (props.type === 'location' && !isAvailable) {
+    return 'Closes at'
+  }
+  if (props.type === 'person' && isAvailable) {
+    return 'Available at'
+  }
+  if (props.type === 'person' && !isAvailable) {
+    return 'Next Available at'
+  }
+}
+
+const result = [
+  {
+    name: 'Ricardo Cooper',
+    isAvailable: true,
+    status: 'Open',
+    time: '8:00am',
+    avatarUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    address: '96 Bass Street, Bungabbee, New South Wales, Australia',
+    businessType: 'Hairdresser',
+    href: '/',
+  },
+  {
+    name: 'Kristen Ramos',
+    isAvailable: true,
+    status: 'Open',
+    time: '9:00am',
+    avatarUrl:
+      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    address: '56 Larissa Court, Neds Corner, Victoria, Australia',
+    businessType: 'Holiday Park',
+    href: '/',
+  },
+  {
+    name: 'Ted Fox',
+    isAvailable: false,
+    status: 'Closed',
+    time: '7:00pm',
+    avatarUrl:
+      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    address: '50 McPherson Road, Tallangatta, Victoria, Australia',
+    businessType: 'Accountant',
+    href: '/',
+  },
+]
+// const result = [
+//   {
+//     name: 'Ricardo Cooper',
+//     isAvailable: true,
+//     status: 'Open',
+//     time: '8:00am',
+//     avatarUrl:
+//       'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//     address: '96 Bass Street, Bungabbee, New South Wales, Australia',
+//     businessType: 'Hairdresser',
+//     href: '/',
+//   },
+//   {
+//     name: 'Kristen Ramos',
+//     isAvailable: true,
+//     status: 'Open',
+//     time: '9:00am',
+//     avatarUrl:
+//       'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//     address: '56 Larissa Court, Neds Corner, Victoria, Australia',
+//     businessType: 'Holiday Park',
+//     href: '/',
+//   },
+//   {
+//     name: 'Ted Fox',
+//     isAvailable: false,
+//     status: 'Closed',
+//     time: '7:00pm',
+//     avatarUrl:
+//       'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//     address: '50 McPherson Road, Tallangatta, Victoria, Australia',
+//     businessType: 'Accountant',
+//     href: '/',
+//   },
+// ]
+</script>
+
 <template>
-  <div class="bg-white shadow overflow-hidden sm:rounded-md">
-    <ul role="list" class="divide-y divide-gray-200">
-      <li v-for="application in applications" :key="application.applicant.email">
-        <a :href="application.href" class="block hover:bg-gray-50">
-          <div class="flex items-center px-4 py-4 sm:px-6">
-            <div class="min-w-0 flex-1 flex items-center">
-              <div class="flex-shrink-0">
-                <img class="h-12 w-12 rounded-full" :src="application.applicant.imageUrl" alt="" />
-              </div>
-              <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
-                <div>
-                  <p class="text-sm font-medium text-indigo-600 truncate">{{ application.applicant.name }}</p>
-                  <p class="mt-2 flex items-center text-sm text-gray-500">
-                    <MailIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-                    <span class="truncate">{{ application.applicant.email }}</span>
-                  </p>
+  <section class="book-container">
+    <p class="text-lg md:text-xl text-gray-800 my-2">{{ props.title }}</p>
+    <div class="bg-white shadow overflow-hidden sm:rounded-md">
+      <ul role="list" class="divide-y divide-gray-200">
+        <li v-for="searchItem in result" :key="searchItem.status">
+          <router-link :to="searchItem.href" class="block hover:bg-gray-50">
+            <div class="flex items-center px-4 py-4 sm:px-6">
+              <div class="min-w-0 flex-1 flex items-center">
+                <div class="flex-shrink-0">
+                  <img class="h-12 w-12 rounded-full" :src="searchItem.avatarUrl" alt="" />
                 </div>
-                <div class="hidden md:block">
+                <div class="min-w-0 flex-1 px-4 md:grid md:grid-cols-2 md:gap-4">
                   <div>
-                    <p class="text-sm text-gray-900">
-                      Applied on
-                      {{ ' ' }}
-                      <time :datetime="application.date">{{ application.dateFull }}</time>
-                    </p>
+                    <p class="text-sm font-medium text-teal-600 truncate">{{ searchItem.name }}</p>
                     <p class="mt-2 flex items-center text-sm text-gray-500">
-                      <CheckCircleIcon class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" aria-hidden="true" />
-                      {{ application.stage }}
+                      <CheckCircleIcon
+                        v-if="searchItem.isAvailable"
+                        class="flex-shrink-0 mr-1.5 h-5 w-5 text-teal-400"
+                        aria-hidden="true"
+                      />
+                      <XCircleIcon v-else class="flex-shrink-0 mr-1.5 h-5 w-5 text-red-400" aria-hidden="true" />
+                      <span class="truncate font-semibold">{{ searchItem.status }}</span>
+                      <span class="mx-2">-</span>
+                      <span class="mr-1">{{ textTimeAvailability(searchItem.isAvailable) }} {{ searchItem.time }}</span>
                     </p>
+                  </div>
+                  <div class="hidden md:block">
+                    <div>
+                      <p class="text-sm text-gray-900">
+                        {{ searchItem.address }}
+                      </p>
+                      <p class="mt-2 flex items-center text-sm text-gray-500">
+                        {{ searchItem.businessType }}
+                      </p>
+                    </div>
                   </div>
                 </div>
               </div>
+              <div>
+                <ChevronRightIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
+              </div>
             </div>
-            <div>
-              <ChevronRightIcon class="h-5 w-5 text-gray-400" aria-hidden="true" />
-            </div>
-          </div>
-        </a>
-      </li>
-    </ul>
-  </div>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+  </section>
 </template>
-
-<script setup lang="ts">
-import { CheckCircleIcon, ChevronRightIcon, MailIcon } from '@heroicons/vue/solid'
-
-const applications = [
-  {
-    applicant: {
-      name: 'Ricardo Cooper',
-      email: 'ricardo.cooper@example.com',
-      imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    date: '2020-01-07',
-    dateFull: 'January 7, 2020',
-    stage: 'Completed phone screening',
-    href: '#',
-  },
-  {
-    applicant: {
-      name: 'Kristen Ramos',
-      email: 'kristen.ramos@example.com',
-      imageUrl:
-        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    date: '2020-01-07',
-    dateFull: 'January 7, 2020',
-    stage: 'Completed phone screening',
-    href: '#',
-  },
-  {
-    applicant: {
-      name: 'Ted Fox',
-      email: 'ted.fox@example.com',
-      imageUrl:
-        'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-    date: '2020-01-07',
-    dateFull: 'January 7, 2020',
-    stage: 'Completed phone screening',
-    href: '#',
-  },
-]
-</script>
