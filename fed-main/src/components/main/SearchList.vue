ff4abb52d252953a6beaa7fc548980a1a69f1ad5
Aleks <aleksandar.trpkovski@gmail.com>
2022-03-14 15:39:41 +1100

    Pagination global component has been create and added to the SearchList

--- fed-main/src/components/main/SearchList.vue
+++ fed-main/src/components/main/SearchList.vue
@@ -6,9 +6,13 @@ export default {
 
 <script setup lang="ts">
 import BookSearchList from '../global/BookSearchList/BookSearchList.vue'
+import BookPagination from '../global/BookPagination/BookPagination.vue'
 </script>
 
 <template>
   <book-search-list title="Locations" type="location" />
   <book-search-list title="People" type="person" />
+  <div class="mt-4">
+    <book-pagination />
+  </div>
 </template>
