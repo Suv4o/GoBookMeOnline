14ffabe90c3094ceaede686f2dc5efcd165126f3
Aleks <aleksandar.trpkovski@gmail.com>
2022-03-16 21:57:32 +1100

    The subscribe users section has been done.

--- fed-main/src/views/HomePage.vue
+++ fed-main/src/views/HomePage.vue
@@ -5,19 +5,21 @@ export default {
 </script>
 
 <script setup lang="ts">
-import HeroSection from '../components/main/HeroSection.vue'
+import HeroSection from '../components/HomePage/HeroSection.vue'
 import BookSearchBar from '../components/global/BookSearchBar/BookSearchBar.vue'
-import SearchList from '../components/main/SearchList.vue'
+import SearchList from '../components/HomePage/SearchList.vue'
+import BookNewsletter from '../components/global/BookNewsletter/BookNewsletter.vue'
 </script>
 
 <template>
   <hero-section />
   <div class="relative">
-    <div class="absolute h-full w-full z-20">
+    <div class="relative w-full z-20">
       <book-search-bar />
     </div>
-    <div class="absolute top-32 sm:top-20 md:top-24 h-full w-full z-10">
+    <div class="relative pt-24 sm:pt-12 md:pt-14 w-full z-10">
       <search-list />
     </div>
   </div>
+  <book-newsletter />
 </template>
