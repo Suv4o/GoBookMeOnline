e1ff04fb19102fefcc6f68f43251858ef07cd7d7
Aleks <aleksandar.trpkovski@gmail.com>
2022-03-19 11:07:30 +1100

    Added code into versioning.ts. Also added a compiler step before executing the code.

--- fed-main/versioning.ts
+++ fed-main/versioning.ts
@@ -1,9 +1,17 @@
-import * as fs from 'fs'
 import simpleGit, { SimpleGit, CleanOptions } from 'simple-git'
+import type { PackageJson } from 'types-package-json'
+import * as fs from 'fs'
 
 const git: SimpleGit = simpleGit().clean(CleanOptions.FORCE)
 
 const rawData = fs.readFileSync('package.json')
-const packageJSON = JSON.parse(rawData.toString())
+const packageJson: Partial<PackageJson> = JSON.parse(rawData.toString())
+const version = packageJson.version
 
-console.log(packageJSON)
+console.log(`Current version: ${version}`)
+
+// try {
+//   git.addAnnotatedTag(version, `Increment version to ${version.substring(1, version.length)}`)
+// } catch (error) {
+//   console.error(error)
+// }
