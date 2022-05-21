96808e3791d59be7e02392e85d3169d52b8183af
Aleks <aleksandar.trpkovski@gmail.com>
2022-05-21 20:48:02 +1000

    Set Nest.js config, add typeORM setup. And validation setup.

--- bed-main/src/app.module.ts
+++ bed-main/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfigAsync } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `env/${process.env.NODE_ENV}.env`,
+    }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
