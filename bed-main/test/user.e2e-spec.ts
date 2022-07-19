22527f0ad2ac7afbd60db03d828497d5fd63ac86
Aleks <aleksandar.trpkovski@gmail.com>
2022-07-19 09:37:04 +0200

    E2E test for creating user with phone number has been done. Still need to be fixed the warning about "Jest did not exit one second after the test run has completed."

--- bed-main/test/user.e2e-spec.ts
+++ bed-main/test/user.e2e-spec.ts
@@ -7,19 +7,31 @@ import { CreateUserWithEmailDto } from '../src/user/dto/create-user-with-email.d
 import { Roles } from '../src/shared/types';
 import { promisify } from 'util';
 import { exec } from 'child_process';
+import * as dotenv from 'dotenv';
+import * as admin from 'firebase-admin';
+import axios from 'axios';
+import { FirebaseAdmin } from '../src/config/firebase.config';
+import { CreateUserWithPhoneDto } from '../src/user/dto/create-user-with-phone.dto';
+const env = dotenv.config({ path: `./env/${process.env.NODE_ENV}.env` });
 const execute = promisify(exec);
 
 describe('User Module (e2e)', () => {
   let app: INestApplication;
+  let firebase: admin.app.App = null;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
+      providers: [FirebaseAdmin],
     }).compile();
 
     app = moduleFixture.createNestApplication();
     setupApp(app);
     await app.init();
+
+    if (!firebase) {
+      firebase = new FirebaseAdmin().setup();
+    }
   });
 
   beforeAll(async () => {
@@ -72,5 +84,61 @@ describe('User Module (e2e)', () => {
         expect(email).toEqual(createUserRequest.email);
         expect(emailVerified).toEqual(false);
       });
+  });
+  it('create user with phone', async () => {
+    try {
+      const userRecord = await firebase.auth().createUser({
+        phoneNumber: '+61411111111',
+      });
+      const createUserRequest: CreateUserWithPhoneDto = {
+        firstName: 'Foo',
+        lastName: 'Bar',
+      };
+      const customToken = await firebase
+        .auth()
+        .createCustomToken(userRecord.uid);
+
+      // Here we're using the custom token to convert it to a Firebase ID token.
+      const token = await axios({
+        method: 'post',
+        url: `https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=${env.parsed.FIREBASE_API_KEY}`,
+        data: {
+          token: customToken,
+          returnSecureToken: true,
+        },
+      });
+
+      const accessToken = token.data.idToken;
+
+      return request(app.getHttpServer())
+        .post('/api/user/signup-phone')
+        .set('Authorization', `Bearer ${accessToken}`)
+        .send(createUserRequest)
+        .expect(201)
+        .then((res) => {
+          const {
+            uid,
+            firstName,
+            lastName,
+            role,
+            phoneNumber,
+            displayName,
+            emailVerified,
+          } = res.body;
+          expect(uid).toBeDefined();
+          expect(firstName).toBeDefined();
+          expect(lastName).toBeDefined();
+          expect(role).toBeDefined();
+          expect(phoneNumber).toBeDefined();
+          expect(displayName).toBeDefined();
+          expect(emailVerified).toBeDefined();
+          expect(firstName).toEqual(createUserRequest.firstName);
+          expect(lastName).toEqual(createUserRequest.lastName);
+          expect(role).toEqual(Roles.USER_DEFAULT);
+          expect(emailVerified).toEqual(false);
+        });
+    } catch (error) {
+      console.log('Error creating new user:', error);
+    }
   });
 });
