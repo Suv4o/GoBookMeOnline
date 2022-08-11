8b6d7980f831a9b171c51231bfd46720b2b60281
Aleks <aleksandar.trpkovski@gmail.com>
2022-08-11 19:32:55 +0200

    Updates to the DefaultMainNav tests. Initialise pinia differently

--- fed-main/src/components/Default/DefaultMainNav/DefaultMainNav.spec.ts
+++ fed-main/src/components/Default/DefaultMainNav/DefaultMainNav.spec.ts
@@ -6,6 +6,8 @@ import DefaultMainNav from './DefaultMainNav.vue'
 import { Auth, getAuth } from 'firebase/auth'
 import * as dotenv from 'dotenv'
 import { initializeFirebaseApp } from '../../../config/firebase.config'
+import { pinia } from '../../../main'
+import { useAuthStore } from '../../../store/auth'
 dotenv.config({ path: `./env/env.${process.env.NODE_ENV}` })
 
 let router: Router
@@ -50,27 +52,49 @@ beforeAll(() => {
   auth = getAuth(initializeFirebaseApp(process.env as ImportMetaEnv))
 })
 
-describe('DefaultMainNav', () => {
+describe('DefaultMainNav', async () => {
   it('render DefaultMainNav component correctly', async () => {
-    render(DefaultMainNav, {
-      global: {
-        plugins: [createTestingPinia({ createSpy: vi.fn }), router],
-        provide: {
-          $auth: auth,
-        },
-      },
-    })
-  })
+    const useStoreAuth = useAuthStore(pinia)
 
-  it('snap shot matches', async () => {
+    // useStoreAuth.user = {
+    //   displayName: 'Foo Bar',
+    //   email: 'test@test.com',
+    //   emailVerified: true,
+    //   firstName: 'Foo',
+    //   lastName: 'Bar',
+    //   phoneNumber: null,
+    //   photoURL: null,
+    //   role: 'USER_DEFAULT',
+    //   uid: 'ZU4jn14tbWfspKuWorshlqQ1s2E3',
+    // }
+
+    // useStoreAuth.isUserReady = true
+
     const wrapper = render(DefaultMainNav, {
       global: {
-        plugins: [createTestingPinia({ createSpy: vi.fn }), router],
+        plugins: [pinia, router],
         provide: {
           $auth: auth,
         },
       },
     })
-    expect(wrapper).toMatchSnapshot()
+
+    const buttonSignOut = wrapper.getAllByText('Sign up')
+    const buttonSignIn = wrapper.getAllByText('Sign in')
+
+    expect(buttonSignOut.length).toBe(1)
+    expect(buttonSignIn.length).toBe(1)
   })
+
+  // it('snap shot matches', async () => {
+  //   const wrapper = render(DefaultMainNav, {
+  //     global: {
+  //       plugins: [createTestingPinia({ createSpy: vi.fn }), router],
+  //       provide: {
+  //         $auth: auth,
+  //       },
+  //     },
+  //   })
+  //   expect(wrapper).toMatchSnapshot()
+  // })
 })
