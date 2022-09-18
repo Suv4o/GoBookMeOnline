1645bd58343df9909b1609201a0567501fbe4de0
Aleks <aleksandar.trpkovski@gmail.com>
2022-09-18 18:11:12 +1000

    Added new script when run test:unit to clean up the vitest store.

--- fed-main/src/config/cli/vitest.setup.ts
+++ fed-main/src/config/cli/vitest.setup.ts
@@ -5,7 +5,8 @@ import * as fs from 'fs'
 
 let accessToken = ''
 
-async function getIdToken() {
+// Get idToken from a user that is created with email link
+async function getIdTokenSigedWitEmailLink() {
   const createdUser = await createUserWithEmail()
   const signedInUser = await signInWithToken(createdUser.customToken)
   return await signedInUser.user?.getIdToken()
@@ -20,21 +21,41 @@ async function writeVitestStore(vitestStore: string) {
   await fs.promises.writeFile('src/config/vitest.store.json', vitestStore)
 }
 
-yargs.command(
-  'setup',
-  'set vitest store',
-  async () => {
-    try {
-      accessToken = await getIdToken()
-      const vitestStoreJson = await readVitestStore()
-      const vitestStore = JSON.parse(vitestStoreJson)
-      vitestStore.EmailVerificationPage.accessToken = accessToken
-      await writeVitestStore(JSON.stringify(vitestStore))
-    } catch (error) {
-      console.log('Error vitest store setup:', error)
+yargs
+  .command(
+    'setup',
+    'set vitest store',
+    async () => {
+      try {
+        // EmailVerificationPage component
+        accessToken = await getIdTokenSigedWitEmailLink()
+        const vitestStoreJson = await readVitestStore()
+        const vitestStore = JSON.parse(vitestStoreJson)
+        vitestStore.EmailVerificationPage.accessToken = accessToken
+        await writeVitestStore(JSON.stringify(vitestStore))
+      } catch (error) {
+        console.log('Error vitest store setup:', error)
+      }
+    },
+    argv => {
+      console.log(argv)
     }
-  },
-  argv => {
-    console.log(argv)
-  }
-).argv
+  )
+  .command(
+    'cleanup',
+    'clean vitest store',
+    async () => {
+      try {
+        // EmailVerificationPage component
+        const vitestStoreJson = await readVitestStore()
+        const vitestStore = JSON.parse(vitestStoreJson)
+        vitestStore.EmailVerificationPage.accessToken = ''
+        await writeVitestStore(JSON.stringify(vitestStore))
+      } catch (error) {
+        console.log('Error vitest store setup:', error)
+      }
+    },
+    argv => {
+      console.log(argv)
+    }
+  ).argv
